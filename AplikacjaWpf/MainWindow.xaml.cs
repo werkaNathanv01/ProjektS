56db3e465615251dac7ed12951f3d57407175581
werkaNathanv01 <117753532+werkaNathanv01@users.noreply.github.com>
2024-06-07 14:00:03 +0200

    Add files via upload

---
+++ AplikacjaWpf/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace AdvancedClicker
+{
+    public partial class MainWindow : Window
+    {
+        private int clickCount = 0;
+        private int clickMultiplierLevel = 1;
+        private int upgradeCost = 10;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            clickCount += clickMultiplierLevel;
+
+            lblClickCount.Content = clickCount.ToString();
+        }
+
+        private void UpgradeMultiplier(object sender, RoutedEventArgs e)
+        {
+            if (clickCount >= upgradeCost)
+            {
+                clickCount -= upgradeCost;
+
+                clickMultiplierLevel++;
+
+                lblClickCount.Content = clickCount.ToString();
+                lblMultiplierLevel.Content = $"{clickMultiplierLevel}x";
+
+                upgradeCost *= 2;
+                ((Button)sender).Content = $"Ulepsz mnożnik (Koszt: {upgradeCost})";
+            }
+            else
+            {
+                MessageBox.Show("Nie masz wystarczającej liczby kliknięć, aby uaktualnić mnożnik.");
+            }
+        }
+    }
+}
